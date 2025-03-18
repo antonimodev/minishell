e5ee8e598e8f2dfd5da5a51d5eeb5b3e76c93e6e
Antonio Moreno <antonio98park@hotmail.com>
2025-03-18 15:55:29 +0000

    updated by makefile

--- src/built-ins/export.c
+++ src/built-ins/export.c
@@ -19,76 +19,100 @@
 // prototipo matrix_replace ->
 // recibe minishell->envp, indice despues de buscar la coincidencia, y lo que quieres meter en ese indice
 
-void matrix_replace(char **matrix, int index, char *new_value)
+static void matrix_replace(char **matrix, int index, char *new_value);
+static char *get_envp_var_name(char *env_var);
+static void replace_env_var(t_minishell *minishell, char *var_name);
+static void process_export_var(t_minishell *minishell);
+
+static void	matrix_replace(char **matrix, int index, char *new_value)
 {
 	free(matrix[index]);
 	matrix[index] = ft_strdup(new_value);
 }
 
-void	foo(t_minishell *minishell)
+static char	*get_envp_var_name(char *env_var)
 {
-	char	**env_matrix;
 	char	*var_name;
-	char	*var_value;
+	int		i;
 
-	// ASIGNACION DE NAME Y VALUE SI EL NOMBRE ES VALIDO
-	if (valid_symbols(minishell->input_matrix[1]))
-	{
-		if (check_strchr_gnl(minishell->input_matrix[1], '='))
-		{
-			env_matrix = ft_split(minishell->input_matrix[1], '=');
-			var_name = env_matrix[0];
-			if (env_matrix[1])
-				var_value = env_matrix[1];
-			else
-				var_value = NULL;
-			free_matrix(env_matrix);
-		}
-	}
-	// CHECKEAR EXISTE ESE NAME?
-	if (ft_getenv(minishell->envp, var_name))
-	{
-		// sustituir nuevo valor con var_value matrix_replace()
-	} else
-		matrix_append(minishell->envp, minishell->input_matrix[1]);
+	i = 0;
+	while (env_var[i] && env_var[i] != '=')
+		i++;
+	var_name = ft_substr(env_var, 0, i);
+	//var_name = ft_strjoin_gnl(var_name, "="); esto era para el nombre con "="
+	return (var_name);
+}
+// probablemente cambiemos el nombre o hagamos algo con la funcionalidad, es literalmente
+// getenv solo que devuelve el índice.
+static int	ft_getenv_index(char **envp, char *env_var)
+{
+    int i;
+    int len;
 
+    i = 0;
+    len = ft_strlen(env_var);
+    while (envp[i])
+    {
+        if (!ft_strncmp(envp[i], env_var, len))
+            return (i);
+        i++;
+    }
+    return (0);
+}
+static void	replace_env_var(t_minishell *minishell, char *var_name)
+{
+	int	index;
+
+	index = ft_getenv_index(minishell->envp, var_name);
+	matrix_replace(minishell->envp, index, minishell->input_matrix[1]);
+}
+
+static void process_export_var(t_minishell *minishell)
+{
+    char *var_name;
+    
+    if (!valid_symbols(minishell->input_matrix[1]))
+    {
+        printf("minishell: export: '%s': not a valid identifier\n", minishell->input_matrix[1]);
+        minishell->exit_status = 1;
+        return ;
+    }
+    if (check_strchr_gnl(minishell->input_matrix[1], '='))
+    {
+        var_name = get_envp_var_name(minishell->input_matrix[1]);
+        if (ft_getenv(minishell->envp, var_name))
+			replace_env_var(minishell, var_name);
+        else
+            minishell->envp = matrix_append(minishell->envp, minishell->input_matrix[1]);
+    }
+    else
+    {
+        var_name = ft_strjoin(minishell->input_matrix[1], "=");
+        if (!ft_getenv(minishell->envp, var_name))
+            minishell->envp = matrix_append(minishell->envp, minishell->input_matrix[1]);
+    }
+	free(var_name);
 }
 
 void ft_export(t_minishell *minishell)
 {
-	int i;
+    int i;
 
-	if (minishell->args_num > 2)
-	{
-		write(1, "\n", 1);
-		return;
-	}
-	if (minishell->args_num == 2)
-	{
-		/*
-			1. Hacer getenv del input_matrix[1]
-			2. export hola=jeje -> hola=jeje
-			3. export hola -> Depende del orden en el que exportemos, se duplica o no.
-				Si hacemos hola=jeje > hola, solo se queda el primero.
-				Si hacemos hola > hola=jeje, se ven las dos, no debería de ocurrir.
-			4. export hola=perro -> Detecta diferencias y crea las dos, debería sustituir su valor.
-		*/
-		// esta condicion es para evitar duplicar la misma variable
-		if (ft_getenv(minishell->envp, minishell->input_matrix[1]))
-			return ;
-		else if (valid_symbols(minishell->input_matrix[1]))
-			minishell->envp = matrix_append(minishell->envp, minishell->input_matrix[1]);
-		else
-		{
-			printf("%s not a valid identifier\n", minishell->input_matrix[1]);
-			minishell->exit_status = 1;
-		}
-		return;
-	}
 	i = 0;
-	while (minishell->envp[i])
-	{
-		printf("declare -x %s\n", minishell->envp[i]);
-		i++;
-	}
+    if (minishell->args_num == 1)
+    {
+        while (minishell->envp[i])
+        {
+            printf("declare -x %s\n", minishell->envp[i]);
+            i++;
+        }
+        return ;
+    }
+    else if (minishell->args_num == 2)
+    {
+        process_export_var(minishell);
+        return ;
+    }
+    else
+        write(1, "\n", 1);
 }
