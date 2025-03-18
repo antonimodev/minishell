2f7423e0bc1d7ba0bc1ad36bbd542856911619c2
Francisco Mariano Ortiz <frmarian@c3r4s4.42malaga.com>
2025-03-18 14:25:45 +0100

    updated by makefile

--- src/built-ins/export.c
+++ src/built-ins/export.c
@@ -6,7 +6,7 @@
 /*   By: frmarian <frmarian@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 13:41:27 by antonimo          #+#    #+#             */
-/*   Updated: 2025/03/18 13:49:57 by frmarian         ###   ########.fr       */
+/*   Updated: 2025/03/18 14:25:26 by frmarian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,44 @@
 // Si hacemos export hola=jeje y luego hacemos export hola=perro debería de sustituirse
 // planteamiento: funcion que checkee si en nuestro minishell->envp existe "hola=" si es así,
 // liberar esa variable de entorno y establecer en la misma posición una nueva con el contenido actualizado
+
+// prototipo matrix_replace ->
+// recibe minishell->envp, indice despues de buscar la coincidencia, y lo que quieres meter en ese indice
+
+void matrix_replace(char **matrix, int index, char *new_value)
+{
+	free(matrix[index]);
+	matrix[index] = ft_strdup(new_value);
+}
+
+void	foo(t_minishell *minishell)
+{
+	char	**env_matrix;
+	char	*var_name;
+	char	*var_value;
+
+	// ASIGNACION DE NAME Y VALUE SI EL NOMBRE ES VALIDO
+	if (valid_symbols(minishell->input_matrix[1]))
+	{
+		if (check_strchr_gnl(minishell->input_matrix[1], '='))
+		{
+			env_matrix = ft_split(minishell->input_matrix[1], '=');
+			var_name = env_matrix[0];
+			if (env_matrix[1])
+				var_value = env_matrix[1];
+			else
+				var_value = NULL;
+			free_matrix(env_matrix);
+		}
+	}
+	// CHECKEAR EXISTE ESE NAME?
+	if (ft_getenv(minishell->envp, var_name))
+	{
+		// sustituir nuevo valor con var_value matrix_replace()
+	} else
+		matrix_append(minishell->envp, minishell->input_matrix[1]);
+
+}
 
 void ft_export(t_minishell *minishell)
 {
@@ -27,6 +65,14 @@ void ft_export(t_minishell *minishell)
 	}
 	if (minishell->args_num == 2)
 	{
+		/*
+			1. Hacer getenv del input_matrix[1]
+			2. export hola=jeje -> hola=jeje
+			3. export hola -> Depende del orden en el que exportemos, se duplica o no.
+				Si hacemos hola=jeje > hola, solo se queda el primero.
+				Si hacemos hola > hola=jeje, se ven las dos, no debería de ocurrir.
+			4. export hola=perro -> Detecta diferencias y crea las dos, debería sustituir su valor.
+		*/
 		// esta condicion es para evitar duplicar la misma variable
 		if (ft_getenv(minishell->envp, minishell->input_matrix[1]))
 			return ;
--- src/validations/valid_symbols.c
+++ src/validations/valid_symbols.c
@@ -2,21 +2,21 @@
 
 bool valid_symbols(char *str)
 {
-    int i;
-    
-    if (!str || !*str)
-        return (false);
-        
-    // Verificar primer carácter (no puede ser un número)
-    if (ft_isdigit(str[0]))
-        return (false);
-        
-    i = 0;
-    while (str[i] && str[i] != '=')
-    {
-        if (!ft_isalnum(str[i]) && str[i] != '_')
-            return (false);
-        i++;
-    }
-    return (true);
+	int i;
+
+	if (!str || !*str)
+	    return (false);
+
+	// Verificar primer carácter (no puede ser un número)
+	if (ft_isdigit(str[0]))
+		return (false);
+
+	i = 0;
+	while (str[i] && str[i] != '=')
+	{
+		if (!ft_isalnum(str[i]) && str[i] != '_')
+			return (false);
+		i++;
+	}
+	return (true);
 }
