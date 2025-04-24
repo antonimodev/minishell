292be4f66f340e14c351284918904e3ac69ab20d
Julia Ortiz Mora <jortiz-m@c3r6s1.42malaga.com>
2025-04-24 13:44:22 +0200

    updated by makefile

--- src/built_ins/echo.c
+++ src/built_ins/echo.c
@@ -3,44 +3,53 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: frmarian <frmarian@student.42malaga.com    +#+  +:+       +#+        */
+/*   By: jortiz-m <jortiz-m@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 13:40:29 by antonimo          #+#    #+#             */
-/*   Updated: 2025/04/24 10:32:09 by frmarian         ###   ########.fr       */
+/*   Updated: 2025/04/24 13:29:01 by jortiz-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	ft_echo(t_minishell *minishell)
+//para que sea valido tiene que ser -n o -n -n -n -n -n
+//invalido -n-n-n-n-n
+
+static bool is_valid(const char *str)
 {
-	int		i;
-	int		j;
-	bool	flag;
+    int i;
 
 	i = 1;
-	j = 1;
-	flag = false;
-	print_minishell(minishell);
-	while (minishell->input_matrix[i]
-		&& !ft_strcmp(minishell->input_matrix[i], "-"))
-	{
-		if (minishell->input_matrix[i][j] == 'n')
-		{
-			flag = true;
-			i++;
-			break;
-		}
-	}
-	printf("Estamos aqui ahora en la matriz: %s\n", minishell->input_matrix[i]);
-	printf("El valor del booleano es: %d\n", flag);
-	while (minishell->input_matrix[i])
-	{
-		printf("%s", minishell->input_matrix[i]);
-		if (minishell->input_matrix[i + 1])
-			printf(" ");
-		i++;
-	}
-	if (!flag)
-		printf("\n");
+    if (str[0] != '-' || str[1] == '\0') // no es valido false
+        return (false);
+    while (str[i]) //mientras que sea n continua "-nnnnnn"
+    {
+        if (str[i] != 'n')
+            return (false);
+        i++;
+    }
+    return (true);
+}
+
+void    ft_echo(t_minishell *minishell)
+{
+    int     i;
+    bool    flag;
+
+    i = 1;
+    flag = false;
+    while (minishell->input_matrix[i] && is_valid(minishell->input_matrix[i])) // si es correcto continua y flg true
+    {
+        flag = true;
+        i++;
+    }
+    while (minishell->input_matrix[i]) //imprimir siguiente de -n
+    {
+        printf("%s", minishell->input_matrix[i]);
+        if (minishell->input_matrix[i + 1]) //si hay siguiente
+            printf(" "); //imprime espacio
+        i++;
+    }
+    if (!flag) //no hay -n que sea valido
+        printf("\n");
 }
